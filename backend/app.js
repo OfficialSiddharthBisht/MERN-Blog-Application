caf8ec76c2739c56ffb33c41ada44bf983edc8a8
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-29 22:39:42 +0530

    CORS-Policy Error Resolved

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+
+// cors policy error resolve
+app.use(cors(
+    {
+        "origin": "*",
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+      }
+));
 
 // Route Imports
 const blog = require("./routes/blogRoute");
