a46fe382cdf934e9b73d49d6b7d4a7f9bcca8bcb
Siddharth <officialsiddharthbisht@gmail.com>
2022-09-29 21:36:45 +0530

    Mongodb database connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const app = require('./app');
-const port = 8000; 
+const dotenv = require('dotenv');
+const connectDatabase = require("./config/database");
+
+// config
+dotenv.config({path:"./config/config.env"});
+
+const port = process.env.PORT || 4000; 
+
+// connecting to database
+connectDatabase();
 
 app.listen(port,()=>{
     console.log(`Server is running on port httttp://localhost:${port}`);
